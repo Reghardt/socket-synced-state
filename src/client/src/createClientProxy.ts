1ea676fdec407350f020dfcd6a052731cec6b1d1
Reghardt <reghardt1000@gmail.com>
2025-05-20 18:32:32 +0200

    restructuring + tsdown

--- src/client/src/createClientProxy.ts
+++ src/client/src/createClientProxy.ts
@@ -1,9 +1,7 @@
 import type { Socket } from "socket.io-client";
-import { useSocketSyncedState } from "./useSocketSyncedState";
+import { useSocketSyncedState } from "../useSocketSyncedState";
 
-type TransformedStatesToHookFunctions<
-  OriginalValueTypesMap extends Record<string, any>
-> = {
+type TransformedStatesToHookFunctions<OriginalValueTypesMap extends Record<string, any>> = {
   [K in keyof OriginalValueTypesMap]: (
     initialValue: OriginalValueTypesMap[K]["initial"]
   ) => [
@@ -11,21 +9,16 @@ type TransformedStatesToHookFunctions<
     (
       updater:
         | OriginalValueTypesMap[K]["type"]
-        | ((
-            prev: OriginalValueTypesMap[K]["type"]
-          ) => OriginalValueTypesMap[K]["type"])
+        | ((prev: OriginalValueTypesMap[K]["type"]) => OriginalValueTypesMap[K]["type"])
     ) => void
   ];
 };
 
-export function createClientProxy<T extends Record<string, any>>(
-  socket: Socket | null
-) {
+export function createClientProxy<T extends Record<string, any>>(socket: Socket | null) {
   return new Proxy({} as TransformedStatesToHookFunctions<T>, {
     get: (target, prop) => {
       if (typeof prop === "string") {
-        return (initialValue: any) =>
-          useSocketSyncedState(socket, prop, initialValue);
+        return (initialValue: any) => useSocketSyncedState(socket, prop, initialValue);
       }
       return undefined;
     },
